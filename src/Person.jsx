9c6933e12e622fd3d681fdbe8235aeaba6be891c
Juan Camilo Bustamante Ramirez <juan.bustamanter@gmail.com>
2023-04-25 00:10:24 -0500

    Birthday app configurado

---
+++ src/Person.jsx
@@ -0,0 +1,11 @@
+const Person = (person) => {
+    const {name, age, image } = person;
+    return <article className="person">
+        <img src={image} alt={name} className="img"/>
+        <div>
+            <h4>{name}</h4>
+            <p>{age} years</p>
+        </div>
+    </article>;
+};
+export default Person;
